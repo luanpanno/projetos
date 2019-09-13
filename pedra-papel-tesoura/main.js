1225953745a6bf9fcc45897b30554c82a876a3a5
luanpanno <luanpanno@gmail.com>
2019-09-12 22:02:43 -0300

    blog viagens atualizado

---
+++ pedra-papel-tesoura/main.js
@@ -0,0 +1,13 @@
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById('user-score');
+const compScore_span = document.getElementById('comp-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_div = document.querySelector('.result');
+const rock_div = document.getElementById('r');
+const paper_div = document.getElementById('p');
+const scissors_div = document.getElementById('s');
+
+rock_div.addEventListener('click', function(){
+    
+})
